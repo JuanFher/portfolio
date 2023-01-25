97c621e8645f84ad6efd05cd657425af60a30051
Juan Fernando <informacion.smartpc@gmail.com>
2023-01-24 19:35:21 -0500

    Add test component for navigation, add livewire component, add migration, factory, model and seeder for navigation items, add blade component for navigation links mobile and web view

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Navitem;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,12 +15,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-     
-
         \App\Models\User::factory()->create([
             'name' => 'JuanFher',
             'email' => 'juanfher@example.com',
             'password' => bcrypt('juanfher'),
+        ]);
+
+        Navitem::factory()->create([
+            'label' => 'Inicio',
+            'link' => '#inicio',
+        ]);
+
+        Navitem::factory()->create([
+            'label' => 'Proyectos',
+            'link' => '#proyectos',
+        ]);
+
+        Navitem::factory()->create([
+            'label' => 'Contacto',
+            'link' => '#contacto',
         ]);
     }
 }
